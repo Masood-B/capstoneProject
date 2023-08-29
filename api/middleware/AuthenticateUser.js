5fe9b440c9f7ca7885d729cff69d5bcde8d50074
Masood-B <masoodbasardien123@gmail.com>
2023-08-29 08:56:42 +0200

    Auth&Err

--- api/middleware/AuthenticateUser.js
+++ api/middleware/AuthenticateUser.js
@@ -1,1 +1,32 @@
-// Authenticate
+const {sign, vertify} = require('jsonwebtoken')
+require('dotenv').config()
+
+function createToken(user){
+    return sign({
+        emailAdd: user.emailAdd,
+        userPass: user.userPass
+    },
+    process.env.SECRET_KEY,
+    {
+        expiresIn: '1h'
+    })
+}
+
+function vertifyAToken(req, res, next){
+    try{
+        console.log("Get token from req.headers['authorization']");
+        const token = req.headers["authorization"]
+        console.log(token);
+        next()
+    }catch(e){
+        res.json({
+            status: res.statusCode,
+            msg: e.message
+        })
+    }
+}
+
+module.exports = {
+    createToken,
+    vertifyAToken
+}
