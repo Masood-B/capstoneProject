4fcea21a1301433e37d0beb62cc008008f9d06cf
Masood-B <masoodbasardien123@gmail.com>
2023-08-30 11:40:33 +0200

    structure

---
+++ cover/src/components/CarouselComp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+          <div id="carouselExampleIndicators" class="carousel slide carousel-fade">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="https://wallpaperset.com/w/full/b/f/2/153494.jpg" class="d-block w-100" alt="..." style="height: 80vh">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://wallpapercave.com/wp/4rCGn9D.jpg" class="d-block w-100" alt="..." style="height: 80vh">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://e1.pxfuel.com/desktop-wallpaper/544/792/desktop-wallpaper-bloodhound-dog-pack-in-pack-of-dogs.jpg" class="d-block w-100" alt="..." style="height: 80vh"   >
+                  </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
