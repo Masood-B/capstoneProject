d10d975a7e047ab4c87992ae628104a9d488bd30
Masood-B <masoodbasardien123@gmail.com>
2023-08-31 16:27:24 +0200

    css

--- cover/src/store/index.js
+++ cover/src/store/index.js
@@ -1,13 +1,43 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+const mangaUrl = 'https://capstoneproject-1ls3.onrender.com/'
 
 export default createStore({
   state: {
+    users: null,
+    user: null,
+    products: null,
+    product: null,
+    spinner: false,
+    token: null,
+    msg: null
   },
   getters: {
   },
   mutations: {
+    setUsers(state, users){
+      state.users = users
+    },
+    setUser(state, user){
+      state.user = user
+    },
+    setProducts(state, products){
+      state.products = products
+    },
+    setProduct(state, product){
+      state.product = product
+    }
+
   },
   actions: {
+    async fetchProducts(context){
+      try{
+        const {data} = await axios.get(`${mangaUrl}products`)
+        context.commit("setProducts", data.results)
+      }catch(e){
+        context.commit("setMsg", "An error occured")
+      }
+    }
   },
   modules: {
   }
--- cover/src/views/ProductsView.vue
+++ cover/src/views/ProductsView.vue
@@ -1,12 +1,65 @@
 <template>
     <div>
-        <h1>test</h1>
+        <div class="container">
+            <div class="row">
+            <div class="filter col-3">
+                <div class="filter">
+                    <h2 class="display-2">Filter</h2>
+                    <div class="row">
+                     <label for="">Adventure</label>
+                     <input type="checkbox" name="" id="">
+                     <label for="">Action</label>
+                     <input type="checkbox" name="" id="">
+                     <label for="">Romance</label>
+                     <input type="checkbox" name="" id="">
+                     <label for="">Horror</label>
+                     <input type="checkbox" name="" id="">
+                     <label for="">Mystery</label>
+                     <input type="checkbox" name="" id="">
+                     <label for="">Slice of Life</label>
+                     <input type="checkbox" name="" id="">
+                    </div>
+                </div>
+            </div>
+            <div class="products col-9">
+                <h2 class="display-2">Products</h2>
+                <button class="btn">Sort</button>
+            <div class="row justify-content-center gap-3" v-if="products">
+                <div class="card" v-for="product in products" style="width: 18rem;" :key="product.prodID">
+                    <img :src="product.prodUrl" class="card-img-top" :alt="product.prodName">
+                    <div class="card-body">
+                      <h5 class="card-title">{{ product.prodName }}</h5>
+                      <p class="card-text">R<span>{{product.amount}}</span></p>
+                    </div>
+                    <div class="card-footer">
+                        <button class="btn">View More</button>
+                        <button class="btn">Buy Now</button>
+                    </div>
+                  </div>
+            </div>
+            <div v-else class="row">
+                <SpinnerComp/>
+            </div>
+        </div>
+        </div>
+    </div>
     </div>
 </template>
 
 <script>
+import SpinnerComp from '../components/SpinnerComp.vue'
     export default {
-        
+        components:{
+            SpinnerComp
+        },
+        computed:{
+            products(){
+                return this.$store.state.products
+            }
+        },
+        mounted(){
+            return this.$store.dispatch('fetchProducts')
+        }
     }
 </script>
 
