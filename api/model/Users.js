1f8bc406de1f153d7c4c5c4b18936c15fd975fa3
Masood-B <129970604+Masood-B@users.noreply.github.com>
2023-09-12 13:58:27 +0200

    token hash update

--- api/model/Users.js
+++ api/model/Users.js
@@ -98,7 +98,7 @@ class Users{
         const data = req.body
         if(data.userPass){
             data.userPass =
-            hashSync(data.userPass, 15)
+            hashSync(data.userPass, 10)
         }
         const query = `
         UPDATE Users
